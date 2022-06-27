3b83df59ea98a3f410600b4c6b95672a03058c73
givenng6 <givenng6@gmail.com>
2022-06-27 09:40:10 +0200

    Floating Buttons

--- src/Stories/MyStories.jsx
+++ src/Stories/MyStories.jsx
@@ -1,17 +1,53 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 
 
 export default function MyStories(){
 
     return(
-    <View>
+    <View style = {styles.Main}>
 
+        <TouchableOpacity style = {styles.FAB_Text}>
+
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.FAB_Media}>
+
+        </TouchableOpacity>
     </View>
     );
 }
 
+const styles = StyleSheet.create({
+    Main:{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1
+    },
+    FAB_Text:{
+        display: 'flex',
+        flexDirection: 'column',
+        bottom: 120, 
+        right: 22,
+        position: 'absolute',
+        backgroundColor: '#0FBFBF',
+        borderRadius: 100,
+        width: 45,
+        height: 45
+    },
+    FAB_Media:{
+        display: 'flex',
+        flexDirection: 'column',
+        bottom: 60, 
+        right: 20,
+        position: 'absolute',
+        backgroundColor: '#0FBFBF',
+        borderRadius: 100,
+        width: 50,
+        height: 50
+    }
+});
 
 
 
